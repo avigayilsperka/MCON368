d28de393cad29165440ab4864d79de82fa218738
Avigayil Sperka <117891420+avigayilsperka@users.noreply.github.com>
2023-02-06 19:36:45 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,88 @@
+﻿/*Avigayil Sperka
+ * T00386137
+ * In class Lab 2
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InclassLab2
+{
+    internal class StudentClass
+    {
+        //underscore implies private
+        private string _studentId;
+        private string _studentName;
+        private decimal _mathScore;
+        private decimal _historyScore;
+        private decimal _scienceScore;
+
+        //properties per each instance variable
+        public string StudentId
+        { get => _studentId; set { _studentId = value; } }    
+            
+        public string StudentName { get=> _studentName; set { _studentName = value; } }
+        public decimal MathScore { 
+            get => _mathScore;
+            set
+            {
+                if (value > 0)
+                {
+                    _mathScore = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error: Invalid math score entry. Score was not set.");
+                }
+            }
+        }
+        public decimal ScienceScore { 
+            get => _scienceScore;
+            set
+            {
+                if (value > 0)
+                {
+                    _scienceScore = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error: Invalid science score entry. Score was not set.");
+                }
+            } 
+        }
+        public decimal HistoryScore {
+            get => _historyScore;
+            set
+            {
+                if (value > 0)
+                {
+                    _historyScore = value;
+                }
+                else
+                    Console.WriteLine("Error: Invalid history score entry. Score was not set.");
+            }
+        }
+        public StudentClass(string studentId, string studentName, decimal mathScore, decimal scienceScore, decimal historyScore)
+        {
+            _studentId = studentId;
+            _studentName = studentName;
+            _mathScore = mathScore;
+            _scienceScore = scienceScore;
+            _historyScore = historyScore;
+        }
+
+        public StudentClass(){}
+
+        public decimal getAggregate()
+        {
+            return _mathScore + _scienceScore + _historyScore;
+        }
+        public decimal getPercentage()
+        {
+            return (getAggregate() / 50) * 100;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*Avigayil Sperka
+ * T00386137
+ * In class Lab 2
+ */
 
-namespace Classwork
+using System;
+using InclassLab2;
+
+
+public class Program
 {
-    internal class HelloWorld 
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
-        }
+        var student1 = new StudentClass();
+        student1.StudentName = "Sam";
+        student1.StudentId = "123abc";
+        student1.MathScore = -90;
+        Console.WriteLine($"-90 set as student math score. Student math score is: {student1.MathScore}");
+        student1.MathScore = 80m;
+        student1.ScienceScore = 95.50m;
+        student1.HistoryScore = 88.75m;
+        Console.WriteLine($"Student's name: {student1.StudentName}");
+        Console.WriteLine($"Student's ID: {student1.StudentId}");
+        Console.WriteLine($"Math score: {student1.MathScore}");
+        Console.WriteLine($"Science score: {student1.ScienceScore}");
+        Console.WriteLine($"History score: {student1.HistoryScore}");
+        Console.WriteLine($"Aggregate score: {student1.getAggregate()}");
+        Console.WriteLine($"Percentage: {student1.getPercentage()}");
+
     }
+
 }
+    
+   
+
+ 
+
+
